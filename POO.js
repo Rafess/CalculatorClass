8236ec4e91927c4bb95cd4a54e7367ab4ab21256
Rafes <rafadvcoliveira@gmail.com>
2021-10-13 22:04:23 -0300

    Fix minus, multiplication and divide classes

--- POO.js
+++ POO.js
@@ -1,58 +1,92 @@
-// Fazer classes  uma para cada operação e uma mae que tenha as funções enter equals reset e list
 class Calculator {
     
     constructor() {
-        this.numStorage = []
+        this.numStorage = [];
+        this.operations = []
+        
     }
-    enter = item => {
-        numStorage = [...numStorage, item]
-       }
-
-        equals = () => {
-        const [firstNum, operator, secondNum] = numStorage;
-        let result =  _calcWard[operator] ? _calcWard[operator](firstNum, secondNum) :
-           "Erro, operação inválida";
-        let finalResult = `${firstNum} ${operator} ${secondNum} = ${result}`;
-        operations = [...operations, finalResult];
-        numStorage = []
-        return finalResult
+    
+    enter(item) {
+        this.numStorage = [...this.numStorage, item];
+  
+    }
+        equals() {
+        this.numStorage = [];
         };
 
-        list = () => {
-            return `Essa é sua lista de opeações até o momento: ${operations}`
+        get list() {
+            return `Essa é sua lista de opeações até o momento: ${this.operations}`
         };
-        reset = () => { 
-            firstNum=0
-            secondNum = 0;
-            numStorage = [];
+
+        reset () { 
+            this.numStorage = [];
+            this.operations = [];
             return (`A calulador foi resetada com sucesso`)
             }
 
 }
+
 class SumCalculator extends Calculator {
     constructor() {
-        
+        super()
+        this.operator = "+";
     }
-sum = (firstNum, secondNum) => firstNum + secondNum;
+        
+        
+       equals()  { 
+        const sum = this.numStorage.reduce((previousSum, currentSum) => previousSum + currentSum, 0);
+        let sumResult = `${this.numStorage.join(' + ')} = ${sum}`
+        console.log(sumResult)
+        this.operations = [...this.operations, sumResult];
+        super.equals()
 }
+}
+        
+
+    
+    
+
 class MinusCalculator extends Calculator {
     constructor() {
-
+        super()
+        this.operator = "-";
     }
-    minus = (firstNum, secondNum) => firstNum - secondNum;
+    
+      equals()  { 
+        const minus = this.numStorage.reduce((previousMin, currentMin) => previousMin - currentMin);
+        let minusResult = `${this.numStorage.join(' - ')} = ${minus}`
+        console.log(minusResult)
+        this.operations = [...this.operations, minusResult];
+        super.equals()
+}
 
 }
+
 class MultiplicateCalculator extends Calculator {
     constructor() {
-
+        super()
+        this.operator = "*";
     }
-    multiplicate = (firstNum, secondNum) => firstNum * secondNum;
-
+          equals()  { 
+        const multiplication = this.numStorage.reduce((previousMult, currentMult) => previousMult * currentMult, 1);
+        let multiplicationResult = `${this.numStorage.join(' * ')} = ${multiplication}`
+        console.log(multiplicationResult)
+        this.operations = [...this.operations, multiplicationResult];
+        super.equals()
 }
+}
+
 class DivideCalculator extends Calculator {
     constructor() {
-
+        super("/")
+        this.operator = "/";
     }
-    divide = (firstNum, secondNum) => firstNum / secondNum;
+         equals()  { 
+        const divide = this.numStorage.reduce((previousDiv, currentDiv) => previousDiv / currentDiv);
+        let divideResult = `${this.numStorage.join(' / ')} = ${divide}`
+        console.log(divideResult)
+        this.operations = [...this.operations, divideResult];
+        super.equals()
+};
 
 }
